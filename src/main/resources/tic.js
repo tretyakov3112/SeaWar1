07ff89a8a57205675e4076e9ba469117e78e74a2
student239 <vAphG2PjmWXuNfVvHpcu>
2022-04-27 12:55:21 +0300

    aaaaaa

--- src/main/resources/tic.js
+++ src/main/resources/tic.js
@@ -1,9 +1,17 @@
 // Function called whenever user tab on any box
-function myfunc() {
+
 
     // Setting DOM to all boxes or input field
-    var b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40, b41, b42, b43, b44, b45, b46, b47, b48, b49, b50, b51, b52, b53, b54, b55, b56, b57, b58, b59, b60, b61, b62, b63, b64, b65, b66, b67, b68, b69, b70, b71, b72, b73, b74, b75, b76, b77, b78, b79, b80, b81, b82, b83, b84, b85, b86, b87, b88, b89, b90, b91, b92, b93, b94, b95, b96, b97, b98, b99,
-        a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45, a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64, a65, a66, a67, a68, a69, a70, a71, a72, a73, a74, a75, a76, a77, a78, a79, a80, a81, a82, a83, a84, a85, a86, a87, a88, a89, a90, a91, a92, a93, a94, a95, a96, a97, a98, a99;
+    let b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23,
+        b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40, b41, b42, b43, b44, b45,
+        b46, b47, b48, b49, b50, b51, b52, b53, b54, b55, b56, b57, b58, b59, b60, b61, b62, b63, b64, b65, b66, b67,
+        b68, b69, b70, b71, b72, b73, b74, b75, b76, b77, b78, b79, b80, b81, b82, b83, b84, b85, b86, b87, b88, b89,
+        b90, b91, b92, b93, b94, b95, b96, b97, b98, b99,
+        a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23,
+        a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45,
+        a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64, a65, a66, a67,
+        a68, a69, a70, a71, a72, a73, a74, a75, a76, a77, a78, a79, a80, a81, a82, a83, a84, a85, a86, a87, a88, a89,
+        a90, a91, a92, a93, a94, a95, a96, a97, a98, a99;
     b0 = document.getElementById("b0").value;
     b1 = document.getElementById("b1").value;
     b2 = document.getElementById("b2").value;
@@ -207,23 +215,9 @@ function myfunc() {
     a99 = document.getElementById("a99").value;
 
 
-    arr1 = [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40, b41, b42, b43, b44, b45, b46, b47, b48, b49, b50, b51, b52, b53, b54, b55, b56, b57, b58, b59, b60, b61, b62, b63, b64, b65, b66, b67, b68, b69, b70, b71, b72, b73, b74, b75, b76, b77, b78, b79, b80, b81, b82, b83, b84, b85, b86, b87, b88, b89, b90, b91, b92, b93, b94, b95, b96, b97, b98, b99,
-    a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45, a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64, a65, a66, a67, a68, a69, a70, a71, a72, a73, a74, a75, a76, a77, a78, a79, a80, a81, a82, a83, a84, a85, a86, a87, a88, a89, a90, a91, a92, a93, a94, a95, a96, a97, a98, a99];
+    let arr1 = [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40, b41, b42, b43, b44, b45, b46, b47, b48, b49, b50, b51, b52, b53, b54, b55, b56, b57, b58, b59, b60, b61, b62, b63, b64, b65, b66, b67, b68, b69, b70, b71, b72, b73, b74, b75, b76, b77, b78, b79, b80, b81, b82, b83, b84, b85, b86, b87, b88, b89, b90, b91, b92, b93, b94, b95, b96, b97, b98, b99,
+        a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45, a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64, a65, a66, a67, a68, a69, a70, a71, a72, a73, a74, a75, a76, a77, a78, a79, a80, a81, a82, a83, a84, a85, a86, a87, a88, a89, a90, a91, a92, a93, a94, a95, a96, a97, a98, a99];
 
-    //arr = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ];
-
-    // Checking if Player X won or not and after
-    // that disabled all the other fields
-
-   /* a = 10;
-    arr = [];
-    function beforeGame(){
-        while (a!=0){
-            k = document.getElementById('print').innerHTML;
-            arr+=k;
-            a--;
-        }
-    }*/
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -231,20 +225,23 @@ function myfunc() {
         return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
     }
    function  randomField(c){
-        //int[][] field = new int[10][10];
-       arr = [];
-        for (let i = 0; i < 10; i++) {
-            k = getRandomInt(0,10);
-            arr[i] = document.getElementById(c+(10*i+k)).value;
-        }
+       let arr = ["", "", "", "", "", "", "", "", "", ""];
+       let k;
+       for (let i = 0; i < 10; i++) {
+           k = getRandomInt(0, 10);
+           arr[i] = arr1[10 * c * i + k];
+       }
         return arr;
     }
 
-    myField = randomField("b");
-    myCount = 10;
-    compField = randomField("a");
-    compCount = 10;
+    let myField = randomField(1);
+    let myCount = 10;
+    let compField = randomField(2);
+    let compCount = 10;
 
+function myfunc() {
+    let element1;
+    let element2;
     for (let i = 0; i < 10; i++) {
         element1 = myField[i];
         element2 = compField[i];
@@ -252,18 +249,15 @@ function myfunc() {
         element2.value = "X";
     }
 
-
-
 }
 
 // Function to reset game
 
 function myfunc_2() {
     location.reload();
-    for (let i = 0; i < 100; i++) {
-        arr1[i] = '';
+    for (let i = 0; i < 200; i++) {
+        arr1[i] = "";
     }
-    //a =10;
 }
 
 
@@ -271,6 +265,7 @@ function myfunc_2() {
 // and put accordingly value X or 0
 flag = 1;
 function stepPlayer(elementId) {
+    myfunc();
     let element = document.getElementById(elementId);
     element.disabled = true;
     for (let i = 0; i < 10; i++) {
@@ -279,25 +274,35 @@ function stepPlayer(elementId) {
             compCount--;
             flag = 0;
             break;
-        } else if (flag == 1 && compField[i] != element && i == 9) {
+        }
+        if (flag == 1 && element.value != "XXX" && i == 9) {
             element.value = "0";
             flag = 0;
         }
     }
 
-    k = getRandomInt(0,10);
-    el = document.getElementById(c+(10*i+k)).value;
+    let k = getRandomInt(0, 10);
+    let el = document.getElementById("b" + (10 * i + k)).value;
     for (let i = 0; i < 10; i++) {
-        if (flag == 0 && myField[i] == element) {
+        if (flag == 0 && myField[i] == el) {
             el.value = "XXX";
-            compCount--;
+            myCount--;
             flag = 1;
             break;
-        } else if (flag == 0 && myField[i] != element && i == 9) {
+        }
+        if (flag == 0 && el.value != "XXX" && i == 9) {
             el.value = "0";
             flag = 1;
         }
     }
 
-    myfunc();
+    if (myCount == 0){
+        document.getElementById('print')
+            .innerHTML = "You lose";
+    }
+    if (compCount == 0){
+        document.getElementById('print')
+            .innerHTML = "You win!";
+    }
+
 }
