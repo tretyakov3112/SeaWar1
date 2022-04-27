efeb719b4b9dfeb088569cf09d55bdd8609eacfb
student239 <vAphG2PjmWXuNfVvHpcu>
2022-04-27 09:11:00 +0300

    doesn't work as well

--- src/main/resources/tic.js
+++ src/main/resources/tic.js
@@ -2,7 +2,9 @@
 function myfunc() {
 
     // Setting DOM to all boxes or input field
-    var b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    var b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40, b41, b42, b43, b44, b45, b46, b47, b48, b49, b50, b51, b52, b53, b54, b55, b56, b57, b58, b59, b60, b61, b62, b63, b64, b65, b66, b67, b68, b69, b70, b71, b72, b73, b74, b75, b76, b77, b78, b79, b80, b81, b82, b83, b84, b85, b86, b87, b88, b89, b90, b91, b92, b93, b94, b95, b96, b97, b98, b99,
+        a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45, a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64, a65, a66, a67, a68, a69, a70, a71, a72, a73, a74, a75, a76, a77, a78, a79, a80, a81, a82, a83, a84, a85, a86, a87, a88, a89, a90, a91, a92, a93, a94, a95, a96, a97, a98, a99;
+    b0 = document.getElementById("b0").value;
     b1 = document.getElementById("b1").value;
     b2 = document.getElementById("b2").value;
     b3 = document.getElementById("b3").value;
@@ -12,94 +14,249 @@ function myfunc() {
     b7 = document.getElementById("b7").value;
     b8 = document.getElementById("b8").value;
     b9 = document.getElementById("b9").value;
+    b10 = document.getElementById("b10").value;
+    b11 = document.getElementById("b11").value;
+    b12 = document.getElementById("b12").value;
+    b13 = document.getElementById("b13").value;
+    b14 = document.getElementById("b14").value;
+    b15 = document.getElementById("b15").value;
+    b16 = document.getElementById("b16").value;
+    b17 = document.getElementById("b17").value;
+    b18 = document.getElementById("b18").value;
+    b19 = document.getElementById("b19").value;
+    b20 = document.getElementById("b20").value;
+    b21 = document.getElementById("b21").value;
+    b22 = document.getElementById("b22").value;
+    b23 = document.getElementById("b23").value;
+    b24 = document.getElementById("b24").value;
+    b25 = document.getElementById("b25").value;
+    b26 = document.getElementById("b26").value;
+    b27 = document.getElementById("b27").value;
+    b28 = document.getElementById("b28").value;
+    b29 = document.getElementById("b29").value;
+    b30 = document.getElementById("b30").value;
+    b31 = document.getElementById("b31").value;
+    b32 = document.getElementById("b32").value;
+    b33 = document.getElementById("b33").value;
+    b34 = document.getElementById("b34").value;
+    b35 = document.getElementById("b35").value;
+    b36 = document.getElementById("b36").value;
+    b37 = document.getElementById("b37").value;
+    b38 = document.getElementById("b38").value;
+    b39 = document.getElementById("b39").value;
+    b40 = document.getElementById("b40").value;
+    b41 = document.getElementById("b41").value;
+    b42 = document.getElementById("b42").value;
+    b43 = document.getElementById("b43").value;
+    b44 = document.getElementById("b44").value;
+    b45 = document.getElementById("b45").value;
+    b46 = document.getElementById("b46").value;
+    b47 = document.getElementById("b47").value;
+    b48 = document.getElementById("b48").value;
+    b49 = document.getElementById("b49").value;
+    b50 = document.getElementById("b50").value;
+    b51 = document.getElementById("b51").value;
+    b52 = document.getElementById("b52").value;
+    b53 = document.getElementById("b53").value;
+    b54 = document.getElementById("b54").value;
+    b55 = document.getElementById("b55").value;
+    b56 = document.getElementById("b56").value;
+    b57 = document.getElementById("b57").value;
+    b58 = document.getElementById("b58").value;
+    b59 = document.getElementById("b59").value;
+    b60 = document.getElementById("b60").value;
+    b61 = document.getElementById("b61").value;
+    b62 = document.getElementById("b62").value;
+    b63 = document.getElementById("b63").value;
+    b64 = document.getElementById("b64").value;
+    b65 = document.getElementById("b65").value;
+    b66 = document.getElementById("b66").value;
+    b67 = document.getElementById("b67").value;
+    b68 = document.getElementById("b68").value;
+    b69 = document.getElementById("b69").value;
+    b70 = document.getElementById("b70").value;
+    b71 = document.getElementById("b71").value;
+    b72 = document.getElementById("b72").value;
+    b73 = document.getElementById("b73").value;
+    b74 = document.getElementById("b74").value;
+    b75 = document.getElementById("b75").value;
+    b76 = document.getElementById("b76").value;
+    b77 = document.getElementById("b77").value;
+    b78 = document.getElementById("b78").value;
+    b79 = document.getElementById("b79").value;
+    b80 = document.getElementById("b80").value;
+    b81 = document.getElementById("b81").value;
+    b82 = document.getElementById("b82").value;
+    b83 = document.getElementById("b83").value;
+    b84 = document.getElementById("b84").value;
+    b85 = document.getElementById("b85").value;
+    b86 = document.getElementById("b86").value;
+    b87 = document.getElementById("b87").value;
+    b88 = document.getElementById("b88").value;
+    b89 = document.getElementById("b89").value;
+    b90 = document.getElementById("b90").value;
+    b91 = document.getElementById("b91").value;
+    b92 = document.getElementById("b92").value;
+    b93 = document.getElementById("b93").value;
+    b94 = document.getElementById("b94").value;
+    b95 = document.getElementById("b95").value;
+    b96 = document.getElementById("b96").value;
+    b97 = document.getElementById("b97").value;
+    b98 = document.getElementById("b98").value;
+    b99 = document.getElementById("b99").value;
 
-    arr1 = [b1,b2,b3,b4,b5,b6,b7,b8,b9];
-    arr = ["","","","","","","","",""];
+    a0 = document.getElementById("a0").value;
+    a1 = document.getElementById("a1").value;
+    a2 = document.getElementById("a2").value;
+    a3 = document.getElementById("a3").value;
+    a4 = document.getElementById("a4").value;
+    a5 = document.getElementById("a5").value;
+    a6 = document.getElementById("a6").value;
+    a7 = document.getElementById("a7").value;
+    a8 = document.getElementById("a8").value;
+    a9 = document.getElementById("a9").value;
+    a10 = document.getElementById("a10").value;
+    a11 = document.getElementById("a11").value;
+    a12 = document.getElementById("a12").value;
+    a13 = document.getElementById("a13").value;
+    a14 = document.getElementById("a14").value;
+    a15 = document.getElementById("a15").value;
+    a16 = document.getElementById("a16").value;
+    a17 = document.getElementById("a17").value;
+    a18 = document.getElementById("a18").value;
+    a19 = document.getElementById("a19").value;
+    a20 = document.getElementById("a20").value;
+    a21 = document.getElementById("a21").value;
+    a22 = document.getElementById("a22").value;
+    a23 = document.getElementById("a23").value;
+    a24 = document.getElementById("a24").value;
+    a25 = document.getElementById("a25").value;
+    a26 = document.getElementById("a26").value;
+    a27 = document.getElementById("a27").value;
+    a28 = document.getElementById("a28").value;
+    a29 = document.getElementById("a29").value;
+    a30 = document.getElementById("a30").value;
+    a31 = document.getElementById("a31").value;
+    a32 = document.getElementById("a32").value;
+    a33 = document.getElementById("a33").value;
+    a34 = document.getElementById("a34").value;
+    a35 = document.getElementById("a35").value;
+    a36 = document.getElementById("a36").value;
+    a37 = document.getElementById("a37").value;
+    a38 = document.getElementById("a38").value;
+    a39 = document.getElementById("a39").value;
+    a40 = document.getElementById("a40").value;
+    a41 = document.getElementById("a41").value;
+    a42 = document.getElementById("a42").value;
+    a43 = document.getElementById("a43").value;
+    a44 = document.getElementById("a44").value;
+    a45 = document.getElementById("a45").value;
+    a46 = document.getElementById("a46").value;
+    a47 = document.getElementById("a47").value;
+    a48 = document.getElementById("a48").value;
+    a49 = document.getElementById("a49").value;
+    a50 = document.getElementById("a50").value;
+    a51 = document.getElementById("a51").value;
+    a52 = document.getElementById("a52").value;
+    a53 = document.getElementById("a53").value;
+    a54 = document.getElementById("a54").value;
+    a55 = document.getElementById("a55").value;
+    a56 = document.getElementById("a56").value;
+    a57 = document.getElementById("a57").value;
+    a58 = document.getElementById("a58").value;
+    a59 = document.getElementById("a59").value;
+    a60 = document.getElementById("a60").value;
+    a61 = document.getElementById("a61").value;
+    a62 = document.getElementById("a62").value;
+    a63 = document.getElementById("a63").value;
+    a64 = document.getElementById("a64").value;
+    a65 = document.getElementById("a65").value;
+    a66 = document.getElementById("a66").value;
+    a67 = document.getElementById("a67").value;
+    a68 = document.getElementById("a68").value;
+    a69 = document.getElementById("a69").value;
+    a70 = document.getElementById("a70").value;
+    a71 = document.getElementById("a71").value;
+    a72 = document.getElementById("a72").value;
+    a73 = document.getElementById("a73").value;
+    a74 = document.getElementById("a74").value;
+    a75 = document.getElementById("a75").value;
+    a76 = document.getElementById("a76").value;
+    a77 = document.getElementById("a77").value;
+    a78 = document.getElementById("a78").value;
+    a79 = document.getElementById("a79").value;
+    a80 = document.getElementById("a80").value;
+    a81 = document.getElementById("a81").value;
+    a82 = document.getElementById("a82").value;
+    a83 = document.getElementById("a83").value;
+    a84 = document.getElementById("a84").value;
+    a85 = document.getElementById("a85").value;
+    a86 = document.getElementById("a86").value;
+    a87 = document.getElementById("a87").value;
+    a88 = document.getElementById("a88").value;
+    a89 = document.getElementById("a89").value;
+    a90 = document.getElementById("a90").value;
+    a91 = document.getElementById("a91").value;
+    a92 = document.getElementById("a92").value;
+    a93 = document.getElementById("a93").value;
+    a94 = document.getElementById("a94").value;
+    a95 = document.getElementById("a95").value;
+    a96 = document.getElementById("a96").value;
+    a97 = document.getElementById("a97").value;
+    a98 = document.getElementById("a98").value;
+    a99 = document.getElementById("a99").value;
+
+
+    arr1 = [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40, b41, b42, b43, b44, b45, b46, b47, b48, b49, b50, b51, b52, b53, b54, b55, b56, b57, b58, b59, b60, b61, b62, b63, b64, b65, b66, b67, b68, b69, b70, b71, b72, b73, b74, b75, b76, b77, b78, b79, b80, b81, b82, b83, b84, b85, b86, b87, b88, b89, b90, b91, b92, b93, b94, b95, b96, b97, b98, b99,
+    a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, a42, a43, a44, a45, a46, a47, a48, a49, a50, a51, a52, a53, a54, a55, a56, a57, a58, a59, a60, a61, a62, a63, a64, a65, a66, a67, a68, a69, a70, a71, a72, a73, a74, a75, a76, a77, a78, a79, a80, a81, a82, a83, a84, a85, a86, a87, a88, a89, a90, a91, a92, a93, a94, a95, a96, a97, a98, a99];
+
+    //arr = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ];
+
     // Checking if Player X won or not and after
     // that disabled all the other fields
+
+   /* a = 10;
+    arr = [];
+    function beforeGame(){
+        while (a!=0){
+            k = document.getElementById('print').innerHTML;
+            arr+=k;
+            a--;
+        }
+    }*/
+
+/*    function int[][] randomField(){
+        //int[][] field = new int[10][10];
+
+        for (let i = 0; i < 10; i++) {
+
+            for (let j = 0; j < 10; j++) {
+                field[i][j] = 0;
+            }
+            Random random = new Random();
+            k = random.nextInt(10);
+            field[i][k] = 1;
+        }
+        return field;
+    }*/
+
     function isWon(c) {
-        if ((b1 == c) && (b2 == c) && (b3 == c)) {
-            return true;
-        }
-        else if ((b7 == c) && (b8 == c) && (b9 == c)) {
-            return true;
-        }
-        else if ((b4 == c) && (b5 == c) && (b6 == c)) {
-            return true;
-        }
-        else if ((b1 == c) && (b4 == c) && (b7 == c)) {
-            return true;
-        }
-        else if ((b2 == c) && (b5 == c) && (b8 == c)) {
-            return true;
-        }
-        else if ((b3 == c) && (b6 == c) && (b9 == c)) {
-            return true;
-        }
-        else if ((b1 == c) && (b5 == c) && (b9 == c)) {
-            return true;
-        }
-        else if ((b3 == c) && (b5 == c) && (b7 == c)) {
-            return true;
-        } else {
-            return false;
-        }
+
     }
 
 
-    if (isWon('X') == true){
-        document.getElementById('print')
-            .innerHTML = "Player X won";
-        window.alert('Player X won');
-    } else if (isWon('0') == true) {
-        document.getElementById('print')
-            .innerHTML = "Player 0 won";
-        window.alert('Player 0 won');
-    }
-        // Checking of Player 0 finsh
-    // Here, Checking about Tie
-    else if ((b1 == 'X' || b1 == '0') && (b2 == 'X'
-            || b2 == '0') && (b3 == 'X' || b3 == '0') &&
-        (b4 == 'X' || b4 == '0') && (b5 == 'X' ||
-            b5 == '0') && (b6 == 'X' || b6 == '0') &&
-        (b7 == 'X' || b7 == '0') && (b8 == 'X' ||
-            b8 == '0') && (b9 == 'X' || b9 == '0')) {
-        document.getElementById('print')
-            .innerHTML = "Match Tie";
-        window.alert('Match Tie');
-    }
-    else {
-
-        // Here, Printing Result
-        if (flag == 1) {
-            document.getElementById('print')
-                .innerHTML = "Player X Turn";
-        }
-        else {
-            document.getElementById('print')
-                .innerHTML = "Player 0 Turn";
-        }
-    }
 }
 
 // Function to reset game
+
 function myfunc_2() {
     location.reload();
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 100; i++) {
         arr1[i] = '';
     }
-
+    a =10;
 }
 
-/*function createField(){
-for (i = 1; i < 4; i++) {
-    i1 = toString(i);
-    s = "b"+i1;
-    print('<input type='+&quot;+'text'+&quot;+' id='+s+' onclick='+
-                       &quot;+'stepPlayer('+s+')'+&quot;+' readonly>')
-    }
-
-}*/
 
 // Here onwards, functions check turn of the player
 // and put accordingly value X or 0
